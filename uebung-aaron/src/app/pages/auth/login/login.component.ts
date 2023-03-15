0db7aaff74232c10e00238dfab1d7c7ac9b22bbd
aaron04648 <75719570+aaron04648@users.noreply.github.com>
2023-03-15 16:59:28 +0100

    updated

--- uebung-aaron/src/app/pages/auth/login/login.component.ts
+++ uebung-aaron/src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ErrorComponent } from 'src/app/shared/Modal/error/error.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   userData = [
     { Firstname: 'admin', Lastname: 'admin' },
     { Firstname: 'test', Lastname: 'test' },
@@ -15,30 +17,40 @@ export class LoginComponent {
   loginValid = true;
   Firstname: any;
   Lastname: any;
-
-  constructor(private _dialog: MatDialog, private _router: Router) {}
-
+  myForm!: FormGroup;
+  constructor(
+    private _dialog: MatDialog,
+    private _router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      Firstname: [null, [Validators.required]],
+      Lastname: [null, [Validators.required]],
+    });
+  }
   login() {
-    // this.userData.forEach((element) => {
-    //   if (
-    //     element.Firstname == this.Firstname &&
-    //     element.Lastname == this.Lastname
-    //   ) {
-    //     let loggedIn = true;
-    //     sessionStorage.setItem('loggedIn', String(loggedIn));
-
-    //     this._router.navigateByUrl('/home');
-
-    //     this._dialog.open(ErrorComponent, {
-    //       data: 'Welcome. Successfully logged in',
-    //     });
-    //   }
-    // });
-
+    console.log(this.userData[0]);
+    console.log(this.myForm.getRawValue());
     let foundUser = this.userData.find(
       (element) =>
-        element.Firstname == this.Firstname && element.Lastname == this.Lastname
+        JSON.stringify(this.myForm.getRawValue()) === JSON.stringify(element)
     );
+
+    if (foundUser) {
+      let loggedIn = true;
+      sessionStorage.setItem('loggedIn', String(loggedIn));
+
+      this._router.navigateByUrl('/home');
+
+      this._dialog.open(ErrorComponent, {
+        data: 'Welcome. Successfully logged in',
+      });
+    } else {
+      this._dialog.open(ErrorComponent, {
+        data: 'Login failed',
+      });
+    }
 
     console.log(foundUser);
   }
--- uebung-aaron/src/app/pages/auth/login/login.module.ts
+++ uebung-aaron/src/app/pages/auth/login/login.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [LoginRoutingModule, CommonModule, MatCardModule],
+  imports: [
+    LoginRoutingModule,
+    CommonModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class LoginModule {}
